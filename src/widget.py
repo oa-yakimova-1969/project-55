d5427ac6161386ab6e774a46dec6cd04135b771b
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-04-25 04:49:28 +0300

    В модуле widget создали функцию get_date

--- src/widget.py
+++ src/widget.py
@@ -11,9 +11,20 @@ def mask_account_card(info: str) -> str:
         identifier = " ".join(parts[:-1])
         number = parts[-1]
         if identifier.lower().startswith("счет"):
-
             masked_info = get_mask_account(number)
             return f"{identifier} {masked_info}"
         else:
             masked_info = get_mask_card_number(number)
             return f"{identifier} {masked_info}"
+
+
+def get_date(date_str: str) -> str:
+    """Функция преобразования формата даты"""
+    date_part = date_str.split("T")[0]  # Берем только дату
+    if date_part:
+        parts = date_part.split("-")
+        new_parts = parts[::-1]
+        new_date = ".".join(new_parts)
+        return new_date
+    else:
+        return "Введены некорректные данные"
